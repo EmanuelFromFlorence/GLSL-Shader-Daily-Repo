62b6850ec401bf5b693a34211072a20d9635b118
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2021-12-28 16:23:43 -0500

    Add files via upload

--- src/shaders/119/fragment.glsl
+++ src/shaders/119/fragment.glsl
@@ -1,5 +1,6 @@
 varying vec2 vUv;
 #define PI 3.14159265358979323846
+#define TWO_PI PI * 2.06
 uniform float u_time;
 
 vec2 Tile(vec2 vUv, float zoom){
@@ -10,23 +11,41 @@ vec2 Tile(vec2 vUv, float zoom){
     return fract(vUv);
 }
 
-vec2 movingTiles(vec2 _st,float _zoom,float _speed){
-    _st*=_zoom;
-    float time=u_time*_speed;
-    if(fract(time)>.5){
-        if(fract(_st.y*.5)>.5){
-            _st.x+=fract(time)*2.;
+vec2 Rot(vec2 vUv, float a){
+    vUv -= 0.5;
+    vUv = mat2(cos(a), -sin(a),
+               sin(a), cos(a)) * vUv;
+    vUv += 0.5;
+    return vUv;
+}
+
+vec2 Tile2(vec2 vUv, float zoom, float speed){
+    vUv *= zoom;
+    float t = u_time * speed;
+    if(fract(t) > .5){
+        if(fract(vUv.y * .5)>.5){
+            vUv.x+= fract(t) * 2.;
+
         }else{
-            _st.x-=fract(time)*2.;
+            vUv.x-= fract(t) * 2.;
         }
-    }else{
-        if(fract(_st.x*.5)>.5){
-            _st.y+=fract(time)*2.;
-        }else{
-            _st.y-=fract(time)*2.;
+        //vUv = Rot(vUv, PI * 0.5);
+    } else {
+        if(fract(vUv.x*.5) > .5){
+            vUv.y += fract(t) * 2.;
+        } else {
+            vUv.y -= fract(t) * 2.;
         }
     }
-    return fract(_st);
+    return fract(vUv);
+}
+
+float Tri(vec2 vUv, float size){
+    vUv -= 0.5;
+    float a = atan(vUv.x, vUv.y) + PI;
+    float r = TWO_PI/3.0;
+    float d = cos(floor(.5 + a/r) * r-a) * length(vUv);
+    return 1.0 - smoothstep(size, size+0.01, d);
 }
 
 float Cir(vec2 vUv, vec2 pos, float size){
@@ -36,8 +55,13 @@ float Cir(vec2 vUv, vec2 pos, float size){
 void main(){
     vec2 vUv = vec2(vUv.x, vUv.y);
     vec3 color = vec3(0.);
-    vUv = movingTiles(vUv, 9.0, 0.25);
+    vUv = Tile2(vUv, 10.0, 0.25);
     float c1 = Cir(vUv, vec2(0.5), 0.25);
-    color = vec3(c1);
+
+    vec2 newUv = vUv;
+    newUv = Rot(vUv, sin(u_time + 2.0) * PI);
+    float t1 = Tri(newUv, 0.1);
+    
+    color = vec3(c1 - t1);
     gl_FragColor = vec4(color, 1.);
 }
