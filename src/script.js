777afbdac10a585cbebe54807651f3071d587ca5
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-04-07 12:20:45 -0400

    shader no.219

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 import './style.css'
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import fragment from './Shaders/218/fragment.glsl'
-import vertex from './Shaders/218/vertex.glsl'
+import fragment from './Shaders/219/fragment.glsl'
+import vertex from './Shaders/219/vertex.glsl'
 const canvas = document.querySelector('.webgl')
 
 class NewScene{
