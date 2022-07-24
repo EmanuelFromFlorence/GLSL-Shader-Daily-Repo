adf853e51c4770708807be2ed3a2ac07ccf7bf1d
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-07-24 07:02:04 -0400

    shader no. 327

---
+++ src/shaders/327/fragment.glsl
@@ -0,0 +1,20 @@
+varying vec2 vUv;
+#define PI 3.14159265359
+#define TWO_PI 6.28318530718
+uniform float u_time;
+uniform vec2 u_mouse;
+
+
+//Shaping functions 1.1
+//plot from book of shaders
+
+float plot(vec2 st, float pct){
+    return smoothstep(pct-0.35, pct, st.y) -
+           smoothstep(pct, pct+0.35, st.y);
+}
+
+void main(){
+    vec2 vUv = vec2(vUv.x, vUv.y);
+    vec3 color = vec3(0.);
+    gl_FragColor = vec4(vUv, 0., 1.);
+}
