9046e61ab696ac45f6e388b0343cb2e5417a031a
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-10-26 11:53:09 -0400

    shader no. 397

--- src/lygia/color/tonemap/linear.glsl
+++
@@ -1,5 +0,0 @@
-#ifndef FNC_TONEMAPLINEAR
-#define FNC_TONEMAPLINEAR
-vec3 tonemapLinear(const vec3 x) { return x; }
-vec4 tonemapLinear(const vec4 x) { return x; }
-#endif
--- src/lygia/color/tonemap/linear.hlsl
+++
@@ -1,5 +0,0 @@
-#ifndef FNC_TONEMAPLINEAR
-#define FNC_TONEMAPLINEAR
-float3 tonemapLinear(const float3 x) { return x; }
-float4 tonemapLinear(const float4 x) { return x; }
-#endif
