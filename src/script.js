76c4304afcadbb19095d0837b991b723eda42bf6
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-10-30 14:32:43 -0400

    shader no. 401

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 import './style.css'
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import fragment from './Shaders/400/fragment.glsl'
-import vertex from './Shaders/400/vertex.glsl'
+import fragment from './Shaders/401/fragment.glsl'
+import vertex from './Shaders/401/vertex.glsl'
 
 const canvas = document.querySelector('.webgl')
 
@@ -18,12 +18,12 @@ class NewScene{
         //this.lygia = new Lygia()
         //console.log(this.time.getElapsedTime())
         this.oldTime = 0
-        this.InitTextShader()
-        //this.InitShader()
+        //this.InitTextShader()
+        this.InitShader()
         this.InitCamera()
         //this.InitLights()
         this.InitRenderer()
-        this.InitControls()
+        //this.InitControls()
         this.Update()
         window.addEventListener('resize', () => {
             this.Resize()
@@ -100,7 +100,7 @@ class NewScene{
 
     InitCamera(){
         this.camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.1, 100)
-        this.camera.position.z = 5
+        this.camera.position.z = 3
         this.scene.add(this.camera)
     }
 
@@ -152,8 +152,8 @@ class NewScene{
             
             this.deltaTime = this.elapsedTime - this.oldTime
             this.oldTime = this.elapsedTime
-            this.controls.update()
-            //this.material.uniforms.u_time.value += this.deltaTime
+            //this.controls.update()
+            this.material.uniforms.u_time.value += this.deltaTime
             if (this.textMaterial){
                 this.textMaterial.uniforms.u_time.value += this.deltaTime
                 //this.textMesh.rotation.y += 0.005
