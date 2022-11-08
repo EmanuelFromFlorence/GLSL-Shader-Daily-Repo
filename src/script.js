91371472ed79d3916d881172eb425f715aa199af
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-11-08 05:42:13 -0500

    shader no. 410

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 import './style.css'
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import fragment from './Shaders/409/fragment.glsl'
-import vertex from './Shaders/409/vertex.glsl'
+import fragment from './Shaders/410/fragment.glsl'
+import vertex from './Shaders/410/vertex.glsl'
 import * as lygia from 'lygia'
 console.log(lygia)
 
