053fdfe44cce307e2a79e7744eb2f0cbb3626203
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-11-10 21:22:01 -0500

    shader no. 412

--- src/shaders/412/fragment.glsl
+++ src/shaders/412/fragment.glsl
@@ -44,25 +44,38 @@ float sdCircle(vec2 p, float r)
     return 1. - smoothstep(0.01, 0.02, x);
 }
 
+float sdSegment(vec2 p, vec2 a, vec2 b)
+{
+    vec2 pa = p-a;
+    vec2 ba = b-a;
+    float h = clamp(dot(pa, ba)/dot(ba,ba), 0., 1.);
+    return length(pa - ba * h);
+}
+
+vec2 Rot(vec2 vUv, float a){
+    vUv -= 0.5;
+    vUv = mat2(cos(a), -sin(a),
+               sin(a), cos(a)) * vUv;
+    vUv += 0.5;
+    return vUv;
+}
+
+
+
 void main()
 {
     vec2 vUv = vec2(vUv.x, vUv.y);
+    //vUv = vUv * 2. - 1.;
+    vec2 vUv2 = vUv;
+    vUv2 = Rot(vUv2, PI * 0.5 );
     vec3 color = vec3(0.);
-    float val1 = (sin(u_time * 0.5)/2.);
-    float val2 = (sin(u_time * 1.0)/3.);
-    float val3 = (sin(u_time * 1.5)/4.);
-    float val4 = (sin(u_time * 2.0)/5.);
-
-    float x = sdRoundedBox(vec2(vUv.x, vUv.y + 0.125), vec2(0.2, 0.125), vec4(0.0, 0.14, 0.0, 0.14));
-    float x1 = sdRoundedBox(vec2(vUv.x, vUv.y + 0.125), vec2(0.15 * (sin(u_time)/5. + 1.), 0.08), vec4(0.0, 0.08, 0.0, 0.08));
-    float y = sdRoundedBoxOutline(vUv, vec2(0.4, 0.45), vec4(0.1, 0.25, 0.1, 0.25), 0.02);
-    float z = sdCircle(vec2(vUv.x - 0.1, vUv.y - 0.1), 0.1);
-    float z1 = sdCircle(vec2(vUv.x - 0.1, vUv.y - 0.1), 0.05);
-    float a = sdCircle(vec2(vUv.x + 0.1, vUv.y - 0.1), 0.1);
-    float a1 = sdCircle(vec2(vUv.x + 0.1, vUv.y - 0.1), 0.05);
-
-    color = vec3(x + y + z + a);
-    color -= z1 + a1 + x1;
-
+    float y =1. - smoothstep(0.01, 0.014, sdSegment(vec2(vUv.x, vUv.y), vec2(0.75 + (sin(u_time)/3.)), vec2(-0.75 + (cos(u_time)/3.))));
+    float x = sdSegment(vec2(vUv.x, vUv.y), vec2(0.75 + (sin(u_time)/3.)), vec2(-0.75 + (cos(u_time)/3.)));
+    float y1 =1. - smoothstep(0.01, 0.014, sdSegment(vec2(vUv2.x, vUv2.y), vec2(0.75 + (cos(u_time)/3.)), vec2(-0.75 + (cos(u_time)/3.))));
+    float x1 = sdSegment(vec2(vUv2.x, vUv2.y), vec2(0.75 + (sin(u_time)/4.)), vec2(-0.75 + (cos(u_time)/4.)));
+    color.g += y;
+    color.r += y1;
+    color.g += x;
+    color.r += x1;
     gl_FragColor = vec4(color, 1.);
 }
